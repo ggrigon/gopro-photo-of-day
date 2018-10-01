40d061da1bbfbeecf5c5fb0868689b0ece657f9a
Guilherme <ggrigon@users.noreply.github.com>
2018-10-01 11:22:08 -0300

    Update goProPhotoOfDay.py

--- goProPhotoOfDay.py
+++ goProPhotoOfDay.py
@@ -4,13 +4,13 @@ import sys
 qtd = int(sys.argv[1])
 url = "https://api.gopro.com/v2/channels/feed/playlists/photo-of-the-day.json?platform=web&page=1&per_page=";
 url = url + str(qtd)
+obj = requests.get(url);
+data = obj.json();
 
 
 for x in range(qtd):
 	print('Baixando a foto {0}' .format(x+1))
 	image_index = x;
-	obj = requests.get(url);
-	data = obj.json();
 	image_name = data['media'][image_index]['permalink']+'.jpg';
 	image_url = data['media'][image_index]['thumbnails']['full']['image'];
 	image_obj = requests.get(image_url);
